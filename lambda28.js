7c2d1c5f635b0ddc0cf63018ecc115e17600963d
Indunil1992 <indunil@adroitlogic.com>
2020-02-28 10:58:27 +0530

    cl trig

--- lambda28.js
+++ lambda28.js
@@ -1,24 +1,6 @@
-let AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
+
 
 exports.handler = async (event) => {
-    try {
-        let data = await ddb.get({
-            TableName: "hirutest",
-            Key: {
-                price: "1",
-                colour: "2"
-            }
-        }).promise();
 
-        console.log("data");
-                console.log(data);
-    } catch (err) {
-        // error handling goes here
-                console.log("err");
-                console.log(err);
-
-    };
-
-    return { "message": "Successfully executed" };
+    return { "message": "Successfully hh executed" };
 };
